39b4ae0d7f74908d3165879f4796551a7affe822
Sachin Arya <sachin.arya@nagarro.com>
2021-03-12 17:43:47 +0530

    initial project setup

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+app.use(bodyParser.json());
+server.applyMiddleware({ app });
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file or main.css file!
+  app.use(express.static('public'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+}
+
+const port = process.env.port || 4000;
+app.listen({ port });
