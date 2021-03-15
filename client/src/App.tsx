447db97d324c5b9b79506557e31b366b6a6d7f3d
Sachin Arya <sachin.arya@nagarro.com>
2021-03-15 11:39:59 +0530

    deployment fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React from here
+          Learn React
         </a>
       </header>
     </div>
--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ server.applyMiddleware({ app });
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file or main.css file!
-  app.use(express.static('public'));
+  app.use(express.static(path.join(__dirname, 'client')));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
   });
 }
 
